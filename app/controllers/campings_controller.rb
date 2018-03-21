57987cbaa96e108b08b465b51ffd4a137a1da742
rodolfo <rodolfo.perezg0@gmail.com>
2018-03-20 23:02:45 -0300

    agregar show de camping

--- app/controllers/campings_controller.rb
+++ app/controllers/campings_controller.rb
@@ -1,10 +1,20 @@
 class CampingsController < ApplicationController
   #before_action :authenticate_user!, only: [:index]
+  before_action :set_campground, only: [:show]
+
   def landing
   end
   def index
     @searching = params[:search]
     searched = Campground.search(@searching)
     @campgrounds = searched.page(params[:page]).per(5)
+  end
+  def show
+
+  end
+
+  private
+  def set_campground
+    @campground = Campground.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'campings/index'
+  resources :campings, only: [:index, :show]
   get 'campings/landing'
   root to: 'campings#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
