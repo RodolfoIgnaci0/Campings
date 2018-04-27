cd8358a589848c707ed5cff9b86801a05b700950
rodolfo <rodolfo.perezg0@gmail.com>
2018-04-26 22:26:06 -0300

    carrierwave y fix elasticsearch

--- app/models/campground.rb
+++ app/models/campground.rb
@@ -3,6 +3,13 @@ class Campground < ApplicationRecord
   belongs_to :user
   paginates_per 50
   searchkick word_middle: [:name, :region]
+
+
+  mount_uploader :photo, PhotoUploader
+  mount_uploader :photo1, Photo1Uploader
+  mount_uploader :photo2, Photo2Uploader
+  mount_uploader :photo3, Photo3Uploader
+  mount_uploader :photo4, Photo4Uploader
   # def self.search(params)
   #   Campground.search(load:true) do
   #     query { string params[:query] if params[:query].present?}
